514e536d406e5ba78d3c5dd54772cd6992260890
Grace Park <parkgra3@gmail.com>
2018-08-06 20:05:57 +0000

    automatically backed up by learn

---
+++ trip_example.rb
@@ -0,0 +1,11 @@
+puts "Let's plan your trip! Where would you like to stay?"
+stay = gets.chomp.capitalize
+
+puts "Where do you want to eat?"
+
+eats = gets.chomp.capitalize
+
+
+
+
+puts "For your trip, you are going to stay in #{stay} and eat at #{eats}."
